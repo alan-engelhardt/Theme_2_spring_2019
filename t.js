b0a556dcced1c55358bbdf5a61e2ea76ae5b097a
Alan Engelhardt <alanengelhardt@Alans-MacBook-2018.local>
2019-11-01 11:08:42 +0100

    add one click button animation

--- t.js
+++ t.js
@@ -1,34 +1,8 @@
-const h1 = document.querySelector("h1");
-const hero = document.querySelector("#hero");
-const flashBtn = document.querySelector("#flash");
-const stopBtn = document.querySelector("#stop");
+const rotbtn = document.querySelector("#rotbtn");
+const boxElement = document.querySelector(".box");
 
-flashBtn.addEventListener("click", startAnimation);
-stopBtn.addEventListener("click", stopAnimation);
+rotbtn.addEventListener("click", animate);
 
-function startAnimation(){
-    hero.classList = "";
-    hero.classList.add("flash");
+function animate(){
+    boxElement.classList.toggle("rot");
 }
-
-function stopAnimation(){
-    hero.classList = "";
-    hero.classList.add("grow");
-}
-
-
-const firstname = "Keld";
-const lastname = "Marple";
-
-let fullname = "Welcome " + firstname + " " + lastname;
-
-console.log(fullname);
-
-h1.textContent = fullname;
-
-fullname = "Alan Engelhardt";
-
-console.log(fullname);
-h1.textContent = fullname;
-
-firstname = "Alan"
